e88d99b08c3d8c57145cb566507eb20ba56c77fa
cesartej53 <cesartej53@gmail.com>
2023-02-03 14:49:43 -0500

    se agrega el proyecto

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    //cambio de header
+    $(window).on('scroll', function(){
+        let posicion = $('#pr').offset()
+        if ($(window).scrollTop()>= posicion.top){
+            $('header').removeClass('transparente')
+        } else{
+            $('header').addClass('transparente')
+        }
+    })
+    //accion de menu lateral
+    $(document).ready(function(){
+        $("#burger").click(function(){
+            $("#burger").toggleClass("abierto");
+            $(".menu").toggleClass("abierto");
+        })
+    })
+    $('a').on('click',function(){
+        if (this.hash !== ''){
+            let hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800)
+        }
+    })
+})
